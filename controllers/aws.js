65e1fdcc438fc65d0fedb7cbc0c5d3aaf58acff3
Hung Luong <silvermoon57@gmail.com>
2017-07-11 21:20:17 +0700

    Fix formatting and add Dimension data

--- controllers/aws.js
+++ controllers/aws.js
@@ -3,60 +3,80 @@ const AWS = require('aws-sdk');
 AWS.config.update({
     accessKeyId: "AKIAIZKFNIVOTCL5CDKA",
     secretAccessKey: "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",
-    "region": "us-east-1"
+    region: "us-east-1"
 });
 var cloudwatch = new AWS.CloudWatch();
 
-exports.sendCloudWatchCount = (name,value) => {	
-	var params = {
-		MetricData: [{
-			"MetricName": name,
-			"Value": value,
-			"Unit": "Count",
-		}],
-		Namespace: "CME"
-	}
-	cloudwatch.putMetricData(params, function(err, data) {
-		if (err)
-			console.log(err, err.stack);
-		else
-			console.log(data);
-        });
+exports.sendCloudWatchCount = (name, value) => {
+    var params = {
+        MetricData: [
+            {
+                MetricName: name,
+                Dimensions: [
+                    {
+                        Name: 'Application',
+                        Value: 'cme-app'
+                    },
+                ],
+                Value: value,
+                Unit: "Count",
+
+            }
+        ],
+
+        Namespace: "CME"
+    }
+    cloudwatch.putMetricData(params, function (err, data) {
+        if (err)
+            console.log(err, err.stack);
+        else
+            console.log(data);
+    });
 };
 
-
-
-exports.sendCloudWatchTime = (name,time) => {
+exports.sendCloudWatchTime = (name, time) => {
     var params = {
-		MetricData: [{
-			"MetricName": name,
-			"Value": time,
-			"Unit": "Seconds",
-		}],
-		Namespace: "CME"
-	}
-    cloudwatch.putMetricData(params, function(err, data) {
-		if (err)
-			console.log(err, err.stack);
-		else
-			console.log(data);
-        });
+        MetricData: [{
+            MetricName: name,
+            Dimensions: [
+                {
+                    Name: 'Application',
+                    Value: 'cme-app'
+                },
+            ],
+            Value: time,
+            Unit: "Seconds",
+        }],
+        Namespace: "CME"
+    }
+    cloudwatch.putMetricData(params, function (err, data) {
+        if (err)
+            console.log(err, err.stack);
+        else
+            console.log(data);
+    });
 };
 
-exports.sendCloudWatchTimeMilis = (name,time) => {
+exports.sendCloudWatchTimeMilis = (name, time) => {
     var params = {
-		MetricData: [{
-			"MetricName": name,
-			"Value": time,
-			"Unit": "Milliseconds",
-		}],
-		Namespace: "CME"
-	}
-    cloudwatch.putMetricData(params, function(err, data) {
-		if (err)
-			console.log(err, err.stack);
-		else
-			console.log(data);
-        });
+        MetricData: [{
+            MetricName: name,
+            Dimensions: [
+                {
+                    Name: 'Application',
+                    Value: 'cme-app'
+                },
+            ],
+            Value: time,
+            Unit: "Milliseconds",
+        }],
+        Namespace: "CME"
+    }
+    cloudwatch.putMetricData(params, function (err, data) {
+        if (err)
+            console.log(err, err.stack);
+        else
+            console.log(data);
+    });
 };
 
